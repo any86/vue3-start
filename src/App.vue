aff96900d03a22ab628a0ca052553b2ad9c0e381
383514580 <383514580@qq.com>
2021-06-27 22:58:24 +0800

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  Counter: {{counter}}
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+
+  data(){
+    return {counter:0}
   }
+
 })
 </script>
 
