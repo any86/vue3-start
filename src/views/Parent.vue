6bac25f13d4c3d80452bec33c99f2d9e606a4a78
Russell <383514580@qq.com>
2021-06-30 16:36:02 +0800

    wip

---
+++ src/views/Parent.vue
@@ -0,0 +1,12 @@
+<template>
+  <Child/>
+  <Child/>
+</template>
+
+<script>
+  import { defineComponent } from "vue";
+  import Child from './Parent/Child.vue'
+  export default defineComponent({
+    components:{Child},
+  });
+</script>
