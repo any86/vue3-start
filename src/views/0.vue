66466e24d94b403544a1faf8a379c2cefd5b04a2
Russell <383514580@qq.com>
2021-07-02 14:02:04 +0800

    补第一个例子

---
+++ src/views/0.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1 :style="{color:color}">{{content}}</h1>
+  <button @click="changeH1">修改</button>
+</template>
+
+<script>
+  import { defineComponent } from "vue";
+  export default defineComponent({
+    data() {
+      return {
+        content: "hi vue",
+        color: "#3eaf7c",
+      };
+    },
+
+    methods: {
+      changeH1() {
+        this.color = "#f00";
+        this.content = "hello world";
+      },
+    },
+  });
+</script>
+
+<style>
+  h1 {
+    font-size: 32px;
+  }
+</style>
