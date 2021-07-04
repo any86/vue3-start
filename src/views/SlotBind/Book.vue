f1436da0fe87291ce4890a8d61a0dbf0e0a3300a
383514580 <383514580@qq.com>
2021-07-04 16:30:50 +0800

    wip

---
+++ src/views/SlotBind/Book.vue
@@ -0,0 +1,16 @@
+<template>
+  <article>
+    <h1 style="color:#3eaf7c;">一本书</h1>
+    <slot v-bind="detail"></slot>
+  </article>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name:'Book',
+  data() {
+    return { detail: { title: "呐喊", author: "鲁迅" } };
+  },
+});
+</script>
