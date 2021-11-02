b843a0402a2898c10dffaf8425e2c536d44c716b
Russell <383514580@qq.com>
2021-11-02 10:49:25 +0800

    child

---
+++ src/components/Child.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 :style="{color}">{{content}}</h1>
+</template>
+
+<script>
+  import { defineComponent } from "vue";
+  export default defineComponent({
+    props:{
+      content:{
+        type:String,
+        default:'hi vue'
+      },
+      
+      color:{
+        type:String,
+        default:'#3eaf7c'
+      }
+    },
+  });
+</script>
---
+++ src/components/Child2.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1 @click="changeColor" :style="{color}">{{content}}</h1>
+</template>
+
+<script>
+  import { defineComponent } from "vue";
+  export default defineComponent({
+    props:{
+      content:{
+        type:String,
+        default:'hi vue'
+      },
+      
+      color:{
+        type:String,
+        default:'#3eaf7c'
+      }
+    },
+    
+    methods:{
+    	changeColor(){
+        this.$emit('change-msg','你好')
+      }
+    }
+  });
+</script>
