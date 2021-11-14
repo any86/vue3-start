c12be7dfede737fc25123480c5040d058328f18e
Russell <383514580@qq.com>
2021-11-14 16:20:48 +0800

    wip

---
+++ src/views/SetupParams.vue
@@ -0,0 +1,17 @@
+<template>
+  <Child v-model:number="n"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Child from './SetupParams/Child.vue'
+export default defineComponent({
+  name: "SetupParams",
+
+  components:{Child},
+
+  data(){
+    return {n:99};
+  },
+});
+</script>
---
+++ src/views/SetupRef.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 ref="titleRef">标题</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent,onMounted,ref } from "vue";
+export default defineComponent({
+  name: "SetupRef",
+
+  setup(){
+    const titleRef = ref(null);
+    onMounted(()=>{
+      console.log(titleRef.value);
+    })
+    return {titleRef}
+  }
+});
+</script>
