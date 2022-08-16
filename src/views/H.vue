e96875d35b31aa61e14055610111d366110b6a00
Russell <383514580@qq.com>
2022-08-16 16:08:54 +0800

    修改vue的cdn地址

---
+++ src/views/H.vue
@@ -0,0 +1,20 @@
+<script>
+import { defineComponent, h } from "vue";
+import ASwitch from "../components/ASwitch.vue";
+export default defineComponent({
+  components: { ASwitch },
+
+  data() {
+    return { checked: false };
+  },
+
+  render() {
+    return h(ASwitch, {
+      checked: this.checked,
+      ["onUpdate:checked"]: (checked) => {
+        this.checked = checked;
+      },
+    });
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
   <router-link class="link" to="/hooks">生命周期钩子(mounted & beforeUnmount)</router-link>
   <router-link class="link" to="/slot-demo">插槽(slot)</router-link>
   <router-link class="link" to="/switch">switch组件</router-link>
-
+  <router-link class="link" to="/h">h函数</router-link>
 </template>
 
 <style lang="scss" scoped>
