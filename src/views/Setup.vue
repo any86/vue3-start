980be76a53ba2232e64b6c94e408c8f07e064234
Russell <383514580@qq.com>
2022-08-18 17:21:56 +0800

    增加setup例子

---
+++ src/views/Setup.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>n : {{ n }}</p>
+  <p>s : {{ s }}</p>
+</template>
+
+<script>
+import { defineComponent, onMounted, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const [s, change] = useS();
+    const n = useN();
+    return { n, s, change };
+  },
+});
+
+function useN() {
+  return "一";
+}
+
+function useS() {
+  const s = ref("你好");
+  function change() {
+    s.value = "hello";
+  }
+  onMounted(() => {
+    change();
+  });
+
+  return [s, change];
+}
+</script>
---
+++ src/views/SetupRef.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>n : {{ n }}</p>
+  <p>s : {{ s }}</p>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  setup() {
+    return { n: ref("1"), s: "你好" };
+  },
+
+  methods: {
+    changeN() {
+      this.n = "一";
+    },
+
+    changeS() {
+      this.s = "hello";
+    },
+  },
+});
+</script>
