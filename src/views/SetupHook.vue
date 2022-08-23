dbc4803da08c763541bc82418b4c48fcefce5d42
Russell <383514580@qq.com>
2022-08-23 10:55:37 +0800

    router例子

---
+++ src/views/SetupHook.vue
@@ -0,0 +1,22 @@
+<template>
+  <p ref="target">目标</p>
+</template>
+
+<script setup>
+import { onMounted, onBeforeUnmount } from "vue";
+import ATouch from "any-touch";
+const target = ref(null);
+let at = null;
+
+// 组件挂载到页面执行的函数.
+onMounted(() => {
+  // 初始化第三方插件
+  at = new ATouch(target.value);
+});
+
+// 组件销毁之前执行的函数.
+onBeforeUnmount(() => {
+  // 销毁第三方插件
+  at.destroy();
+});
+</script>
---
+++ src/views/SetupRouter.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+const route = useRoute();
+const router = useRouter();
+</script>
+
+<template>
+
+</template>
